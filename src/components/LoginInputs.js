45bcd623f0b7d0de5ee9f5aab85b011e32bb572a
anankebj <anankebj@gmail.com>
2022-11-16 13:19:15 -0600

    adding components folder

---
+++ src/components/LoginInputs.js
@@ -0,0 +1,13 @@
+export function Login() {
+  return (
+    <div>
+      <h1>Inicia sesión</h1>
+      <h3>Correo electronico</h3>
+      <input class="email-input" type="email"></input>
+      <h3>Contraseña</h3>
+    </div>
+  );
+}
+export function Form() {
+  return <h1> hola</h1>;
+}
