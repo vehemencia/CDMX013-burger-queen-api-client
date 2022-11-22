06e68e531b18f0fe3df0b9b723644b9264d45073
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-11-22 14:00:31 -0600

    Add styles to ticket and products section

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,17 +1,15 @@
-import './Ticket.css'
+import './Ticket.css';
+import { OrderInformation } from '../OrderInformation/OrderInformation';
 
 export const Ticket = () => {
   return (
     <div className="ticket-with-client-order">
       <div className="waitress-name">Atiende: </div>
-      <div className="ticket-section">Cant. &nbsp; Producto &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Costo</div>
+      <div className="ticket-section">Cant. &nbsp;Producto &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Costo</div>
       <div className="order-in-ticket">
-        <div className="q-of-products"></div>
-        <div className="product-name"></div>
-        <div className="cost-of-products"></div>
-        <div className="delete-from-order"></div>
+      {OrderInformation()}
       </div>
-      <div className="total-cost">Total: </div>
+      <div className="total-cost">Total: $30</div>
     </div>
   )
 }
