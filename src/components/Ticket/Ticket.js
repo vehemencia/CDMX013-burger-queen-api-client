a644f9adc44aeb382b5dd1ca47b6957b27e653ae
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-11-22 12:01:25 -0600

    Add ticket components

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -2,16 +2,16 @@ import './Ticket.css'
 
 export const Ticket = () => {
   return (
-  <div className="ticket-with-client-order">
-    <div className="waitress-name">Atiende: </div>
-    <div className="ticket-section">Cant.    Producto     Costo</div>
-    <div className="order-in-ticket">
-      <div className="q-of-products"></div>
-      <div className="product-name"></div>
-      <div className="cost-of-products"></div>
-      <div className="delete-from-order"></div>
+    <div className="ticket-with-client-order">
+      <div className="waitress-name">Atiende: </div>
+      <div className="ticket-section">Cant. &nbsp; Producto &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Costo</div>
+      <div className="order-in-ticket">
+        <div className="q-of-products"></div>
+        <div className="product-name"></div>
+        <div className="cost-of-products"></div>
+        <div className="delete-from-order"></div>
+      </div>
+      <div className="total-cost">Total: </div>
     </div>
-    <div className="total-cost">Total: </div>
-  </div>
   )
 }
