7b4af90d5f2c9149141709f11908d9c6e680ae2c
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-12-01 12:09:41 -0600

    Add css module to login display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //import { Routes, Route } from "react-router-dom";
 import { LoginInputs } from './components/LoginInputs/LoginInputs.js';
-import { WaitressSelection } from './components/WaitressSelection/WaitressSelection.js'
-import { OrderMenu } from './components/OrderMenu/OrderMenu.js';
+//import { WaitressSelection } from './components/WaitressSelection/WaitressSelection.js'
+//import { OrderMenu } from './components/OrderMenu/OrderMenu.js';
 //import { AdminSelection } from './components/AdminSelection/AdminSelection.js'
 
 const router = createBrowserRouter([
@@ -10,14 +10,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <LoginInputs/>,
   },
-  {
-    path: "/meseros",
-    element: <WaitressSelection/>,
-  },
-  {
-    path: "/meseros/crear-orden",
-    element: <OrderMenu/>,
-  },
+  // {
+  //   path: "/meseros",
+  //   element: <WaitressSelection/>,
+  // },
+  // {
+  //   path: "/meseros/crear-orden",
+  //   element: <OrderMenu/>,
+  // },
 ]);
 
 function App() {
@@ -25,11 +25,5 @@ function App() {
     <RouterProvider router={router} />
   );
 }
-
-// function App() {
-//   return (
-//     <LoginInputs/>
-//   );
-// }
 
 export default App;
--- src/components/LoginInputs/LoginInputs.js
+++ src/components/LoginInputs/LoginInputs.js
@@ -1,18 +1,18 @@
 import { EnterOrSendButton } from '../EnterOrSendButton/EnterOrSendButton.js';
-import './LoginInputs.css';
+import styles from './LoginInputs.module.css'
 
 export const LoginInputs = () => {
   return (
-    <main className='login-content'>
-      <div className='img-div'>
-        <img src='https://iili.io/H9lsuOx.png' id='logo-img' alt='logo'/>
+    <main className={styles.loginContent}>
+      <div className={styles.imgDiv}>
+        <img src='https://iili.io/H9lsuOx.png' id={styles.logoImg} alt='logo'/>
       </div>
-      <div className='inputs-div'>
-        <h2 id='login-text'>Inicia sesión</h2>
-        <h3 className='login-label'>Correo electrónico</h3>
-        <input className='login-input' type='email'></input>
-        <h3 className='login-label'>Contraseña</h3>
-        <input className='login-input' type='password'></input>
+      <div className={styles.inputsDiv}>
+        <h2 id={styles.loginText}>Inicia sesión</h2>
+        <h3 className={styles.loginLabel}>Correo electrónico</h3>
+        <input className={styles.loginInput} type='email'></input>
+        <h3 className={styles.loginLabel}>Contraseña</h3>
+        <input className={styles.loginInput} type='password'></input>
         <EnterOrSendButton buttonText='ENTRAR'/>
       </div>
     </main>
